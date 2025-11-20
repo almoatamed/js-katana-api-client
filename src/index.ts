eec84398de537208c33f174d002f420573b0ba9b
Salem Elmotamed <s.elmotamed96@gmail.com>
2025-11-20 04:47:38 +0200

    refactor: enhance create function to support adapter types and improve type safety

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { createApiClient } from "./api-client/index.js";
+import { ApiProps, createApiClient, Merge } from "./api-client/index.js";
 import type { ApiDelete, ApiGet, ApiPost, ApiPut, AsyncEmit, OnEvent } from "../apiTypes.js";
 export * from "./api-client/index.js";
 
-const create = createApiClient<ApiPost, ApiPut, ApiDelete, ApiGet, AsyncEmit, OnEvent>;
+const create = <Adapter extends "fetch" | "axios">(
+    apiProps: Merge<
+        {
+            adapter?: Adapter;
+        },
+        ApiProps
+    >
+) => createApiClient<Adapter, ApiPost, ApiPut, ApiDelete, ApiGet, AsyncEmit, OnEvent>(apiProps);
 
 export default create;
